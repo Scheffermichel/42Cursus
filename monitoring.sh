692685e49e1f3cef2773c437c0744556ff177c60
Michel Scheffer sales <mscheffe@c2r6s1.42lisboa.com>
2023-01-30 20:39:17 +0000

    complete

--- monitoring.sh
+++ monitoring.sh
@@ -5,16 +5,20 @@ disk_total=$(df -Bg | grep '^/dev/' | awk '{dt += $2} END {printf("%dGb"), dt}')
 disk_used=$(df -Bm | grep '^/dev/' | awk '{du += $3} END {print du}')
 disk_percent=$(df -Bm | grep '^/dev/' | awk '{dt += $2} {du += $3} END {printf("%d"), du/dt*100}')
 cpu_load=$(top -bn1 | grep ^%Cpu | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1+$3}')
-
+last_boot=$(who -b | awk '{print $3,$4}')
+lvm_use=$(lsblk | grep lvm | awk '{if ($6) {print "yes";exit;} else {print "no";exit;} }')
+tcp=$(netstat | grep '^tcp' | grep 'ESTABLISHED$' | wc -l)
+mac=$(ip a | grep link/ether | awk '$1 == "link/ether" {print $2}')
+sudo=$(cat /var/log/sudo/sudolog | grep COMMAND | wc -l)
 echo "	#Architecture: $(uname -a)
     	#CPU physical : $(nproc)
 	#vCPU : $virtual_cpu 
 	#Memory Usage: $memoryu
 	#Disk Usage: $disk_used/$disk_total ($disk_percent%)
 	#CPU load: $cpu_load
-	#last boot:
-	#LVM use:
-	#Connections TCP :
-	#User log:
-	#Network:
-	#Sudo :"
+	#last boot: $last_boot
+	#LVM use: $lvm_use
+	#Connections TCP : $tcp ESTABLISHED
+	#User log: $(users | wc -w)
+	#Network: IP $(hostname -I) ($mac)
+	#Sudo : $sudo cmd"
